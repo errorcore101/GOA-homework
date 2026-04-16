5eaaf85d56d46b925f7ef6c573f104d3cad52047
errorcore101 <vakho713@icloud.com>
2026-04-16 13:47:20 +0400

    commit

---
+++ day080/homework/hw.js
@@ -0,0 +1,35 @@
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U");
+}
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function squareSum(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i] * numbers[i];
+  }
+  return sum;
+}
+
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
